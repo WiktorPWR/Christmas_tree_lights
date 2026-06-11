9e29c21bc210f9ce104c5bcb2a0204d7645cf12d
WiktorPWR <wiktor.jankowskiostrowski@globallogic.com>
2026-06-11 16:32:46 +0200

    Usage of buttons implemented

--- stm32-hal/Chrismas_led_lights/Core/Src/timer_functions.c
+++ stm32-hal/Chrismas_led_lights/Core/Src/timer_functions.c
@@ -9,7 +9,28 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
         {
             // Button is still pressed
             actual_button_pressed.pressed_confirmation = 1;
-            
+            if(actual_button_pressed.GPIO_Pin == Button_up_Pin){
+                LED_blinking_speed_changer_by_const_value();
+            }
+            else if(actual_button_pressed.GPIO_Pin == Button_down_Pin){
+                LED_blinking_speed_decrement_by_const_value();
+            }
+            else if(actual_button_pressed.GPIO_Pin == Button_left_Pin){
+                LED_brightness_increment_by_const_value();
+            }
+            else if(actual_button_pressed.GPIO_Pin == Button_rigth_Pin){
+                LED_brightness_decrement_by_const_value();
+            }
+            else if(actual_button_pressed.GPIO_Pin == Button_middle_Pin){
+                //This will on and off the LEDs
+                if(htim4.Init.Period == 0){
+                    htim4.Init.Period = 1000;
+                }
+                else{
+                    htim4.Init.Period = 0;
+                }
+                // Handle Button Middle action
+            }
         }
         else
         {
